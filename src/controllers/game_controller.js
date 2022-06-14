8a569bd22d610d77ef103a96055504b812d568cc
11Some1 <68611574+11Some1@users.noreply.github.com>
2022-06-14 16:51:52 -0400

    init game functionality

--- src/controllers/game_controller.js
+++ src/controllers/game_controller.js
@@ -9,7 +9,11 @@ function makeGameId(length) {
   return str;
 }
 
-const createGame = async (socket, req) => {
+function floorMod(n, m) {
+  return Math.floor(((n % m) + m) % m);
+}
+
+const createGame = async (socket, io, req) => {
   const gameId = makeGameId(4);
 
   await gameService.updateGame(
@@ -17,6 +21,8 @@ const createGame = async (socket, req) => {
     'false', // active
     req.id, // hostId
     [req.id], // clients
+    [], // alivePlayers
+    [], // deadPlayers
     '0', // turnIdx
     '0', // reportedRoll
     '0', // dice1,
@@ -25,7 +31,7 @@ const createGame = async (socket, req) => {
     '', // prevPlayer
     '1', // MIN_NUM_PLAYERS
     '8', // MAX_NUM_PLAYERS
-    'roll-stage', // curStage
+    'before-roll-stage', // curStage
     '', // turnResult
     '0' // pressedOk
   );
@@ -53,7 +59,7 @@ const createGame = async (socket, req) => {
   });
 };
 
-const joinGame = async (socket, req) => {
+const joinGame = async (socket, io, req) => {
   if (!await gameService.existsGame(req.gameId)) {
     socket.emit('joinGame', {
       success: false
@@ -76,7 +82,7 @@ const joinGame = async (socket, req) => {
         gameId: req.gameId,
         active: gameData.active,
         isHost: (req.id === gameData.hostId),
-        isTurn: (req.id === gameData.currentPlayer),
+        isTurn: (req.id === gameData.currentPlayerId),
         clients: clientInfo,
         currentPlayerId: gameData.currentPlayerId,
         prevPlayerId: gameData.prevPlayerId,
@@ -99,13 +105,18 @@ const joinGame = async (socket, req) => {
       gameData.active,
       gameData.hostId,
       gameData.clients = gameData.clients.concat([req.id]),
+      gameData.alivePlayers,
+      gameData.deadPlayers,
       gameData.turnIdx,
       gameData.reportedRoll,
-      gameData.actualRoll,
+      gameData.dice1,
+      gameData.dice2,
       gameData.currentPlayerId,
       gameData.prevPlayerId,
       gameData.MIN_NUM_PLAYERS,
       gameData.MAX_NUM_PLAYERS,
+      gameData.curStage,
+      gameData.turnResult,
       gameData.pressedOk,
     );
 
@@ -135,9 +146,118 @@ const joinGame = async (socket, req) => {
   }
 };
 
+const initGame = async (socket, io, req) => {
+  if (!await gameService.existsGame(req.gameId)) {
+    socket.emit('initGame', {
+      success: false
+    });
+    return;
+  }
+
+  const gameData = await gameService.getGame(req.gameId);
+  const clientInfo = await gameService.getClientInfo(req.gameId);
+
+  if (req.id !== gameData.hostId || gameData.MIN_NUM_PLAYERS > gameData.clients.length) {
+    socket.emit('initGame', {
+      success: false
+    });
+    return;
+  }
+
+  await gameService.updateGame(
+    gameData.gameId,
+    gameData.active = true,
+    gameData.hostId,
+    gameData.clients,
+    gameData.alivePlayers = gameData.clients,
+    gameData.deadPlayers,
+    gameData.turnIdx = Math.floor(Math.random() * gameData.alivePlayers.length),
+    gameData.reportedRoll = 0,
+    gameData.dice1 = 0,
+    gameData.dice2 = 0,
+    gameData.currentPlayerId = gameData.alivePlayers[gameData.turnIdx],
+    gameData.prevPlayerId = gameData.alivePlayers[floorMod(gameData.turnIdx - 1, gameData.alivePlayers.length)],
+    gameData.MIN_NUM_PLAYERS,
+    gameData.MAX_NUM_PLAYERS,
+    gameData.curStage = 'before-roll-stage',
+    gameData.turnResult = '',
+    gameData.pressedOk = 0,
+  );
+
+  gameData.clients.forEach((id) => {
+    io.to(id).emit('initGame', {
+      success: true,
+      gameId: req.gameId,
+      active: gameData.active,
+      isHost: (req.id === gameData.hostId),
+      isTurn: (req.id === gameData.currentPlayerId),
+      clients: clientInfo,
+      currentPlayerId: gameData.currentPlayerId,
+      prevPlayerId: gameData.prevPlayerId,
+      reportedRoll: gameData.reportedRoll,
+      dice1: gameData.dice1,
+      dice2: gameData.dice2,
+      curStage: gameData.curStage,
+      turnResult: gameData.turnResult,
+    });
+  });
+};
+
+const nextRound = async (socket, io, req) => {
+  if (!await gameService.existsGame(req.gameId)) {
+    socket.emit('nextRound', {
+      success: false
+    });
+    return;
+  }
+
+  const gameData = await gameService.getGame(req.gameId);
+  const clientInfo = await gameService.getClientInfo(req.gameId);
+
+  await gameService.updateGame(
+    gameData.gameId,
+    gameData.active = true,
+    gameData.hostId,
+    gameData.clients,
+    gameData.alivePlayers,
+    gameData.deadPlayers,
+    gameData.turnIdx = Math.floor(Math.random() * gameData.alivePlayers.length),
+    gameData.reportedRoll = 0,
+    gameData.dice1 = 0,
+    gameData.dice2 = 0,
+    gameData.currentPlayerId = gameData.alivePlayers[gameData.turnIdx],
+    gameData.prevPlayerId = gameData.alivePlayers[floorMod(gameData.turnIdx - 1, gameData.alivePlayers.length)],
+    gameData.MIN_NUM_PLAYERS,
+    gameData.MAX_NUM_PLAYERS,
+    gameData.curStage = 'before-roll-stage',
+    gameData.turnResult = '',
+    gameData.pressedOk = 0,
+  );
+
+  gameData.clients.forEach((id) => {
+    io.to(id).emit('nextRound', {
+      success: true,
+      gameId: req.gameId,
+      active: gameData.active,
+      isHost: (req.id === gameData.hostId),
+      isTurn: (req.id === gameData.currentPlayerId),
+      clients: clientInfo,
+      currentPlayerId: gameData.currentPlayerId,
+      prevPlayerId: gameData.prevPlayerId,
+      reportedRoll: gameData.reportedRoll,
+      dice1: gameData.dice1,
+      dice2: gameData.dice2,
+      curStage: gameData.curStage,
+      turnResult: gameData.turnResult,
+    });
+  });
+};
+
 const gameController = {
   createGame,
-  joinGame
+  joinGame,
+  initGame,
+  nextRound,
 };
 
 export default gameController;
