6a299a017c2e31f173ef462f411b2ed235ec6ac7
11Some1 <68611574+11Some1@users.noreply.github.com>
2022-06-13 23:03:51 -0400

    organize db further

--- src/routers/game_router.js
+++ src/routers/game_router.js
@@ -2,10 +2,10 @@ import gameController from '../controllers/game_controller';
 
 export const gameRouter = async (socket, req) => {
   switch (req.url) {
-    case 'create_game':
+    case 'createGame':
       await gameController.createGame(socket, req);
       break;
-    case 'join_game':
+    case 'joinGame':
       await gameController.joinGame(socket, req);
       break;
     default:
